5b11584b38d368940066020585a0783b5d1eb553
MuhammadHafijur <hafijur.salah@gmail.com>
2021-10-18 19:23:46 +0600

    added environment variable

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCASVYI3a4CI8XYIjgJ-O7Doj9CXqU5EtA",
-  authDomain: "mediku-health.firebaseapp.com",
-  projectId: "mediku-health",
-  storageBucket: "mediku-health.appspot.com",
-  messagingSenderId: "1019825288366",
-  appId: "1:1019825288366:web:2bc94d26ece5350588a93f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
 };
 
 export default firebaseConfig;
